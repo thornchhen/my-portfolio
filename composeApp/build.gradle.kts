64b48662c3c720e32d8c81c582e469b6dc6736a2
Thorn Chhen <92152423+thornchhen@users.noreply.github.com>
2026-09-16 00:08:39 +0700

    Add composeApp/build.gradle.kts

---
+++ composeApp/build.gradle.kts
@@ -0,0 +1,24 @@
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+
+plugins {
+    kotlin("multiplatform")
+    id("org.jetbrains.compose")
+    id("org.jetbrains.kotlin.plugin.compose")
+}
+
+kotlin {
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmJs {
+        outputModuleName = "portfolio"
+        browser { commonWebpackConfig { outputFileName = "portfolio.js" } }
+        binaries.executable()
+    }
+    sourceSets {
+        commonMain.dependencies {
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.material3)
+            implementation(compose.ui)
+        }
+    }
+}
