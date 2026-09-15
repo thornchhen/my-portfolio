b3475d118fbee1aac3fc284d9fde94f8f0404f0a
Thorn Chhen <92152423+thornchhen@users.noreply.github.com>
2026-09-16 00:07:54 +0700

    Add build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,5 @@
+plugins {
+    kotlin("multiplatform") version "2.3.20" apply false
+    id("org.jetbrains.compose") version "1.12.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.3.20" apply false
+}
